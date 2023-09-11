eaa3a6fb3ab588547866b639795442716cf4929e
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-09-11 17:48:51 -0400

    added auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+
 import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
     title: 'Spotify Clone',
     description: 'Find some bops',
 }
@@ -18,9 +22,15 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={font.className}>
-                <Sidebar>
-                    {children}
-                </Sidebar>
+                <ToasterProvider />
+                <SupabaseProvider>
+                    <UserProvider>
+                        <ModalProvider />
+                        <Sidebar>
+                            {children}
+                        </Sidebar>
+                    </UserProvider>
+                </SupabaseProvider>
             </body>
         </html>
     )
